9c451da6e4f5f5a4d425728e6ed0a1d086a4dddb
YOUR NAME <jhanov@gmail.com>
2023-05-16 20:12:01 -0500

    adds html table & loop through currency to fill in currency table on front end

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>  Currency Table </h2> 
+
+      <table class="table table-striped">
+  <thead>
+    <tr>
+      <th>Id </th>
+      <th> Name  </th>
+      <th> Symbol </th>
+      <th> Market Cap (USD) </th> 
+      <th> Price (USD) </th> 
+      <th> Change % in 24 Hours </th> 
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="currency in currencies">
+       <th scope="row">{{ currency.id }}</th>  
+       <td>{{ currency.name }}</td> 
+       <td>{{  currency.symbol }}</td>  
+       <td>{{  currency.market_cap_usd }} </td>
+       <td>{{  currency.price_usd }} </td>
+       <td>{{  currency.change_percent_24hr }} </td> 
+    </tr>
+   </tbody>
+</table> 
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+<style>
+table,
+th,
+td {
+  border: 1px solid black;
 }
+</style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      currencies: [],
+    };
+  },
+  created: function () {
+    this.indexCurrencies();
+  },
+  methods: {
+    indexCurrencies: function () {
+      axios.get("/currencies").then((response) => {
+        console.log("currencies loaded", response);
+        this.currencies = response.data;
+      });
+    },
+  },
+};
 </script>
